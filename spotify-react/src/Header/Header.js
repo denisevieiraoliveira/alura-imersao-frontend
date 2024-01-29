ae58a3915a7d579ae6a91346be307b4e203a030d
denisevieiraoliveira <denise.vieira90@gmail.com>
2024-01-29 14:14:15 -0300

    commit-29/01

--- spotify-react/src/Header/Header.js
+++ spotify-react/src/Header/Header.js
@@ -7,24 +7,26 @@ import search from '../assets/search.png';
 const Header = () => {
     return (
         <nav className="header__navigation">
-            <div className="navigation">
-                <button className="arrow-left">
-                    <img src={smallLeft} alt="Seta esquerda" />
-                </button>
-                <button className="arrow-right">
-                    <img src={smallRight} alt="Seta direita" />
-                </button>
-            </div>
-            <div className="header__search">
-                <img src={search} alt="Buscar" />
-                <input
-                    id="search-input"
-                    maxLength="800"
-                    autoCorrect="off"
-                    autoCapitalize="off"
-                    spellCheck="false"
-                    placeholder="O que você quer ouvir?"
-                />
+            <div className="arrows-search">
+                <div className="navigation">
+                    <button className="arrow-left">
+                        <img src={smallLeft} alt="Seta esquerda" />
+                    </button>
+                    <button className="arrow-right">
+                        <img src={smallRight} alt="Seta direita" />
+                    </button>
+                </div>
+                <div className="header__search">
+                    <img src={search} alt="Buscar" />
+                    <input
+                        id="search-input"
+                        maxLength="800"
+                        autoCorrect="off"
+                        autoCapitalize="off"
+                        spellCheck="false"
+                        placeholder="O que você quer ouvir?"
+                    />
+                </div>
             </div>
             <div className="header__login">
                 <button className="subscribe">Inscreva-se</button>
